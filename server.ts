d094eb0c4fc3020ed950785012c3ed3b8b1e9563
Utkarsh <utkjaiswal58@gmail.com>
2024-09-22 15:50:31 +0530

    ufrontend url added to avoid cors policy error

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(
     cors({
-      origin: ['http://localhost:3000',"*"],
+      origin: ['http://localhost:3000',"https://firstbench-ai-react.vercel.app"],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
     }),
